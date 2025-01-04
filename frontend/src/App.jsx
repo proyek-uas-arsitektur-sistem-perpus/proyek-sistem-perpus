be9ff2d365ce17c63bb944d819727ddfaf222ed8
cantikawijaya <cantikawjy@gmail.com>
2025-01-04 18:56:25 +0800

    memperbaiki import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,19 +3,11 @@ import Login from "./pages/Login";
 import Logout from "./pages/Logout";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
-<<<<<<< HEAD
 import Dashboardpengguna from "./pages/Dashboardpengguna";
-import GantiPassword from './components/GantiPassword';
+import GantiPassword from './pages/GantiPassword';
 import SidebarStaff from "./components/SidebarStaff";
-// import SidebarPengguna from "./components/SidebarPengguna"; // Import SidebarPengguna
 import SearchFilter from './components/SearchFilter';
 import ProfilePengguna from "./pages/ProfilePengguna";  // Import halaman Profile
-=======
-import GantiPassword from "./pages/GantiPassword";
-import SidebarStaff from "./components/SidebarStaff";
-import SearchFilter from "./components/SearchFilter";
-import ProfilePengguna from "./pages/ProfilePengguna";
->>>>>>> 9ffdf8ac696fd2fa754c011c797a5904588d03eb
 import DataBuku from "./pages/Databuku";
 import TambahBuku from "./pages/Tambahbuku";
 import DataPengguna from "./pages/DataPengguna"; // Import halaman Data Pengguna
@@ -28,7 +20,6 @@ const App = () => {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-<<<<<<< HEAD
           
           {/* Dashboard untuk Staff */}
           <Route path="/dashboard" element={
@@ -46,17 +37,6 @@ const App = () => {
             </div>
           } />
           
-=======
-          <Route
-            path="/dashboard"
-            element={
-              <div>
-                <SidebarStaff />
-                <Dashboard />
-              </div>
-            }
-          />
->>>>>>> 9ffdf8ac696fd2fa754c011c797a5904588d03eb
           <Route path="/login/change-password" element={<GantiPassword />} />
           <Route path="/lupa-password" element={<GantiPassword />} />
           <Route
